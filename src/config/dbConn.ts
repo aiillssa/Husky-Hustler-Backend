cc56037d730bb1efffe217d241dfcee6625a9496
Aaditya Thukral <aadityathukral2003@gmail.com>
2024-10-29 14:06:30 -0700

    ready to start routes

--- src/config/dbConn.ts
+++ src/config/dbConn.ts
@@ -2,7 +2,7 @@
 // Write the function to set up connection w/ SQL database
 // This will be called at the start in server.ts
 
-import { AppDataSource } from "../data-source";
+import { AppDataSource } from "./data-source";
 
 /** Method to connect to the database */
 export const connectDB = async () => {
