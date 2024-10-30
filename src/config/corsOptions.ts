43350536f89263ec36cb603a2c767e5fb72bf21b
Aaditya Thukral <aadityathukral2003@gmail.com>
2024-10-30 15:44:19 -0700

    trying again to figure out github actions

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,6 +1,7 @@
 const allowedOrigins = ["http://localhost:3000"];
 
 // Use a function to check if the request origin is allowed
+// :OOOOOOO
 export const corsOptions = {
   origin: allowedOrigins,
 };
