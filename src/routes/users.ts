cda918cd700de08697369c1aa4f01a1e2c3e2875
Aaditya Thukral <aadityathukral2003@gmail.com>
2024-11-10 12:39:18 -0800

    Get User

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,11 +4,13 @@ import {
   updateUser,
   createUser,
   deleteUser,
+  getUser,
 } from "../controllers/userController";
 import { createUserValidator } from "../middleware/validators/createUserValidator";
 const routerUser: Router = express.Router();
 
 routerUser.post("/", createUserValidator, createUser);
 routerUser.get("/", getAllUsers);
+routerUser.get("/:id", getUser);
 routerUser.delete("/:id", deleteUser);
 export default routerUser;
