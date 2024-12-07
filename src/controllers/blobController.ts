af2847622e49d6ac20a7e3f9773f7573f064a6a5
ailsa <ailsa.chen@outlook.com>
2024-12-07 15:05:48 -0800

    i hope this will work for multi images (blobController.ts, uploadproductblob updates)

--- src/controllers/blobController.ts
+++ src/controllers/blobController.ts
@@ -101,30 +101,17 @@ export const uploadBlob = async (req: Request, res: Response) => {
     const blobServiceClient = await loadServiceClient();
     const containerClient = blobServiceClient.getContainerClient("images");
 
-    //here is where i will add the names and stuff
-
-
-    //const imagePath = "C:\\CSE 403\\Husky-Hustler-Backend\\kittyThinking.jpg"
-
-    //I THINK THIS IS HOW IT WILL B BUT IDK gotta wait until frontend 
-
     const file = req.file as MulterFile
     const id = req.body.id;
     const source = req.body.source;
-
-    // for (let pair of req.file.entries()) {
-    //     console.log(pair[0], pair[1]);  // Logs the key and value
-    // }
 
 
     console.log("body", req.body);
     console.log("file received", file.originalname);
 
     const blockBlobClient = containerClient.getBlockBlobClient(id + "-" + source)
-    //const buffer = await fs.readFile(imagePath)
 
     try {
-        //await blockBlobClient.uploadStream(formData)
         await blockBlobClient.uploadData(file.buffer);
         res.status(200).send("[blobController] - successfully uploaded blob")
     } catch (err) {
@@ -132,11 +119,44 @@ export const uploadBlob = async (req: Request, res: Response) => {
         res.status(500).send("An error occurred while uploading the image.");
     }
 
-    //res.status(200).send("[blobController] - successfully uploaded blob")
+}
+
+export const uploadProductBlob = async (req: Request, res: Response) => {
+
+    console.log("Initializing BlobServiceClient...");
+    const blobServiceClient = await loadServiceClient();
+    const containerClient = blobServiceClient.getContainerClient("images");
+
+    const files = req.files as Express.Multer.File[];
+
+    const id = req.body.id;
+    const source = req.body.source;
+    const caption = req.body.caption as string[];
+    const price = req.body.price as string[];
+    console.log(caption)
+    console.log(price)
+
+    files.forEach(async (file, index) => {
+        const blockBlobClient = containerClient.getBlockBlobClient(id + "-" + source + index)
+        try {
+            await blockBlobClient.uploadData(file.buffer);
+            res.status(200).send("[blobController] - successfully uploaded blob")
+        } catch (err) {
+            console.log(err)
+            res.status(500).send("An error occurred while uploading the image.");
+        }
+
+    });
+
+
+
+
+
+    // console.log("body", req.body);
+    // console.log("file received", file.originalname);
+
+
 
 }
 
-function multer() {
-    throw new Error("Function not implemented.");
-}
 
--- src/routes/blob.ts
+++ src/routes/blob.ts
@@ -1,15 +1,20 @@
 import express from "express";
 
-import { downloadBlob, listBlobs, uploadBlob } from "../controllers/blobController";
+import { downloadBlob, listBlobs, uploadBlob, uploadProductBlob } from "../controllers/blobController";
 import multer from "multer";
 const routerBlobs = express.Router();
 
 const upload = multer();
 
+// const uploadFields = upload.fields([
+//     {name: 'files'}, {name: 'captions'}, {name: 'prices'}
+// ])
+
 
 //routerBlobs.get("/:id/:source", downloadBlob) //will b added in the future...
 routerBlobs.get("/:id/:source", downloadBlob);
 routerBlobs.post("/", upload.single('file'), uploadBlob);
+routerBlobs.post("/product", upload.array('files'), upload.array('captions'), upload.array('prices'), uploadProductBlob)
 
 export default routerBlobs;
 
