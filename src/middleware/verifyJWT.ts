dc8aaf27eb3e100760cfb157f44a29f3d0b8bcc2
Sean0z <seanozinsky@gmail.com>
2024-12-08 16:43:00 -0800

    Added comments to verifyJWT

--- src/middleware/verifyJWT.ts
+++ src/middleware/verifyJWT.ts
@@ -1,8 +1,21 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
+// App secret key to sign and verify JWTs 
 const SECRET_KEY = process.env.APP_SECRET!;
 
+/**
+ * Verifys and validates auth JWTs for protected routes and automatically refreshes expired tokens if possible
+ * @param req 
+ * - Contains the authorization token in the authorization token and the refresh token in cookies for refreshing
+ * @param res 
+ * - The status code as well as the new auth token if old auth token was expired and the refresh token was valid
+ * @param next
+ * - If the auth token is valid or it was sucessfully refreshed, proceed to called endpoint
+ * @returns 
+ * - 400 if the auth token is invalid
+ * - 401 if the refresh token is missing, is invalid or expired, or the authorization header is missing
+ */
 export const verifyJWT = (req: Request, res: Response, next: NextFunction): void => {
   try {
     const authHeader = req.headers.authorization;
@@ -26,7 +39,7 @@ export const verifyJWT = (req: Request, res: Response, next: NextFunction): void
           const newAuthToken = refreshJWT(refreshToken);
 
           if (!newAuthToken) {
-            return res.status(401).json({ error: 'Invalid or expired refresh token AAAA' });
+            return res.status(401).json({ error: 'Invalid or expired refresh token' });
           }
 
           // Set new token in response headers
@@ -52,6 +65,14 @@ export const verifyJWT = (req: Request, res: Response, next: NextFunction): void
   }
 };
 
+/**
+ * Creates a new auth JWT using a vaild refresh JWT
+ * @param refreshToken 
+ * - The refresh token to use to create a new auth token
+ * @returns 
+ * - A new auth JWT with a 5 minute expiration if the refresh token is valid
+ * - null if the refresh token is invalid or expired
+ */
 const refreshJWT = (refreshToken: string): string | null => {
   try {
     const decodedRefresh = jwt.verify(refreshToken, SECRET_KEY) as JwtPayload;
